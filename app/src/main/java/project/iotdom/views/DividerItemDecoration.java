377e46f95e926d55b280c8c181773ef5c26d179e
JMazur121 <mrsilviril@gmail.com>
2018-06-08 11:56:24 +0200

    Small fixes, few changes in the UI

---
+++ app/src/main/java/project/iotdom/views/DividerItemDecoration.java
@@ -0,0 +1,6 @@
+package project.iotdom.views;
+
+import android.support.v7.widget.RecyclerView;
+
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+}
---
+++ app/src/main/java/project/iotdom/views/VerticalSpaceItemDecoration.java
@@ -0,0 +1,6 @@
+package project.iotdom.views;
+
+import android.support.v7.widget.RecyclerView;
+
+public class VerticalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+}
